3572361041b3406bc14c838d23e5dea6e186cbb4
dhrubmahajan17 <dhrubmahajan17@gmail.com>
2026-03-30 14:54:29 +0530

    urls done

--- resume-builder-backend/accounts/views.py
+++ resume-builder-backend/accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        return Response({"message": "Register endpoint working!"})
+
+class ProfileView(APIView):
+    def get(self, request):
+        return Response({"message": "Profile endpoint working!"})
